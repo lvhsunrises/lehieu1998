642bf43929b300db78474c0627cf5097237fb025
lvhsunrises <lvhsunrises@gmail.com>
2019-07-31 12:47:35 +0700

    upload bai tap

--- baitap_web/baitap_web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="baitap_web.MvcApplication" Language="C#" %>
---
+++ web_baitap/web_baitap/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace web_baitap
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/plugins/jquery-ui/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/plugins/validate/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/plugins/bootstrap/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/plugins/bootstrap/bootstrap.min.css",
+                      "~/Content/plugins/style/style.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
